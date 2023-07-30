b8a481c8525a170b7e31f74151525d7ea224dbd0
HRITHWIKJRAJ <hrithwik.j@gmail.com>
2023-07-30 16:46:25 +0530

    sample

--- src/App.tsx
+++ src/App.tsx
@@ -4,47 +4,15 @@ import { Notifications } from "react-push-notification";
 import addNotification from "react-push-notification";
 
 function App() {
-  // const express = require("express");
-  // const bodyParser = require("body-parser");
-  // const AWS = require("aws-sdk");
+  const express = require("express");
+  const bodyParser = require("body-parser");
+  const AWS = require("aws-sdk");
 
-  // AWS.config.update({
-  //   region: "ap-south-1", // e.g., 'us-east-1'
-  // });
+  AWS.config.update({
+    region: "ap-south-1", // e.g., 'us-east-1'
+  });
 
-  // const sns = new AWS.SNS();
-  // const app = express();
-  // app.use(bodyParser.json());
-
-  // app.post(
-  //   "/subscribe",
-  //   async (
-  //     req: { body: { endpoint: any; topicArn: any } },
-  //     res: { sendStatus: (arg0: number) => void }
-  //   ) => {
-  //     const { endpoint, topicArn } = req.body;
-
-  //     const params = {
-  //       Protocol: "https", // Replace with the appropriate protocol for your PWA
-  //       TopicArn: topicArn,
-  //       Endpoint: endpoint,
-  //     };
-
-  //     try {
-  //       const response = await sns.subscribe(params).promise();
-  //       console.log("Subscription ARN:", response.SubscriptionArn);
-  //       res.sendStatus(200);
-  //     } catch (error) {
-  //       console.error("Error subscribing to SNS topic:", error);
-  //       res.sendStatus(500);
-  //     }
-  //   }
-  // );
-
-  // const port = process.env.PORT || 3001;
-  // app.listen(port, () => {
-  //   console.log(`Server listening on port ${port}`);
-  // });
+  const sns = new AWS.SNS();
 
   const buttonClick = () => {
     addNotification({
@@ -52,14 +20,36 @@ function App() {
       subtitle: "This is a subtitle",
       message: "This is a very long message",
       theme: "darkblue",
-      // native: true, // when using native, your OS will handle theming.
+      native: true, // when using native, your OS will handle theming.
     });
+  };
+  const subscribe = async () => {
+    const params = {
+      Protocol: "https", // Replace with the appropriate protocol for your PWA
+      TopicArn: "arn:aws:sns:ap-south-1:363892094207:testTopic",
+      Endpoint: "https://3.110.208.152",
+    };
+
+    try {
+      const response = await sns.subscribe(params).promise();
+      console.log("Subscription ARN:", response.SubscriptionArn);
+      console.log("response", response);
+    } catch (error) {
+      console.error("Error subscribing to SNS topic:", error);
+      console.log("response", error);
+    }
   };
 
   return (
-    <div className="App">
-      <button onClick={buttonClick}>send notification</button>
-      <Notifications />
+    <div>
+      <div className="App">
+        <button onClick={buttonClick}>send notification</button>
+        <Notifications />
+      </div>
+      <div className="App">
+        <button onClick={subscribe}>subscribe</button>
+        <Notifications />
+      </div>
     </div>
   );
 }
