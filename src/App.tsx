3c199bad1b6bee5e9247ac4c7a0e16080fc72939
HRITHWIK JAYARAJAN RAJ <hrithwik.jayarajan_raj@mercedes-benz.com>
2023-08-08 10:19:52 +0530

    service worker

--- src/App.tsx
+++ src/App.tsx
@@ -4,51 +4,46 @@ import { Notifications } from "react-push-notification";
 import addNotification from "react-push-notification";
 
 function App() {
-  const AWS = require("aws-sdk");
 
-  AWS.config.update({
-    region: "ap-south-1", // e.g., 'us-east-1'
-    accessKeyId: process.env.REACT_APP_ACCESS_KEY,
-    secretAccessKey: process.env.REACT_APP_ACCESS_SECRET,
+
+  Notification.requestPermission((status) =>{
+    console.log("permission status: ",status)
   });
 
-  const sns = new AWS.SNS();
+
+  function displayNotification(){
+    const options = {
+      body:'notification body',
+      vibrate:[100,50,100]
+    }
+    if (Notification.permission === 'granted') {
+      navigator.serviceWorker.getRegistration().then( reg => {
+        reg?.showNotification('Hello World',options);
+      });
+    }
+  }
+
+
+  displayNotification();
+
 
   const buttonClick = () => {
-    console.log(process.env.REACT_APP_ACCESS_KEY , process.env.REACT_APP_ACCESS_SECRET);
-    addNotification({
-      title: "Warning",
-      subtitle: "This is a subtitle",
-      message: "This is a very long message",
-      theme: "darkblue",
-      native: true, // when using native, your OS will handle theming.
-    });
-  };
-  const subscribe = async () => {
-    const params = {
-      Protocol: "https", // Replace with the appropriate protocol for your PWA
-      TopicArn: "arn:aws:sns:ap-south-1:363892094207:testTopic",
-      Endpoint: "https://3.110.208.152",
-    };
-
-    try {
-      const response = await sns.subscribe(params).promise();
-      console.log("Subscription ARN:", response.SubscriptionArn);
-      console.log("response", response);
-    } catch (error) {
-      console.error("Error subscribing to SNS topic:", error);
-      console.log("response", error);
-    }
+    
+    displayNotification();
+    // console.log(process.env.REACT_APP_ACCESS_KEY , process.env.REACT_APP_ACCESS_SECRET);
+    // addNotification({
+    //   title: "Warning",
+    //   subtitle: "This is a subtitle",
+    //   message: "This is a very long message",
+    //   theme: "darkblue",
+    //   native: true, // when using native, your OS will handle theming.
+    // });
   };
 
   return (
     <div>
       <div className="App">
         <button onClick={buttonClick}>send notification</button>
-        <Notifications />
-      </div>
-      <div className="App">
-        <button onClick={subscribe}>subscribe</button>
         <Notifications />
       </div>
     </div>
